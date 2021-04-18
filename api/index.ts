c70f1e2d09fee4fe95e98092a9a2df79f972130d
THiragi <w1th1nmybr41n@gmail.com>
2021-04-18 16:04:13 +0900

    implement scraping function with puppeteer

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,26 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = "World" } = request.query;
-  response.status(200).send(`Hello ${name}!`);
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const { url = "https:takahira.io" } = req.query;
+  const urlString = typeof url === "string" ? url : url[0];
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+  const page = await browser.newPage();
+  await page.goto(urlString);
+  const dimensions = await page.evaluate(() => ({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
+    title: document.title,
+    deviceScaleFactor: window.devicePixelRatio,
+  }));
+  console.log("Dimensions:", dimensions);
+
+  await browser.close();
+
+  res.send(`a page title is ${dimensions.title}`);
 };
